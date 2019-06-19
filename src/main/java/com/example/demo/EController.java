b57b268e53fd7f2f6dce14d65041f2b11915ed93
ishanpatel123 <46852098+ishanpatel123@users.noreply.github.com>
2019-06-19 18:03:21 -0400

    Enable Cross Origin for controller

--- src/main/java/com/example/demo/EController.java
+++ src/main/java/com/example/demo/EController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("emp")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EController {
 	
 	@Autowired
